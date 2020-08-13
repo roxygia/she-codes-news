5c11df9e3d6b5749910fea66c98bbc03c35d496e
roxygia <62426542+roxygia@users.noreply.github.com>
2020-08-13 23:32:52 +0800

    added category to  create story form

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
